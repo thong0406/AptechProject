e3ec8efc31a9aa00a4b3e7f8b072d3f594277387
hoangngoc-lan <90963233+hoangngoc-lan@users.noreply.github.com>
2022-01-01 21:34:54 +0700

    Delete admin directory

--- admin/layout/style.blade.php
+++
@@ -1,9 +0,0 @@
-<link href="{{ asset("admin_template/vendor/fontawesome-free/css/all.min.css") }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="{{ asset("admin_template/css/sb-admin-2.min.css") }}" rel="stylesheet">
-<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
-<link rel="stylesheet" type="text/css" href="{{ asset("admin_template/css/styleadd.css") }}">
