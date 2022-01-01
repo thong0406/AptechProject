6737b1dbc24aed64adb730934c7529ff8894362f
hoangngoc-lan <90963233+hoangngoc-lan@users.noreply.github.com>
2022-01-01 21:14:35 +0700

    Add files via upload

---
+++ bakery/layout/script.blade.php
@@ -0,0 +1,13 @@
+ <script src="/bakery/js/vendor/jquery-1.12.0.min.js"></script>
+        <script src="/bakery//jquery-easing/jquery.easing.min.js"></script>
+        <script src="/bakery/js/bootstrap.min.js"></script>
+        <script src="/bakery/js/parallax.min.js"></script>
+        <script src="/bakery/js/ajax-mail.js"></script>
+        <script src="/bakery/js/owl.carousel.min.js"></script>
+        <script src="/bakery/js/jquery.nicescroll.min.js"></script>
+        <script src="/bakery/js/main.js"></script>
+        <script src="/bakery/js/slick.min.js"></script>
+         <script src="/bakery/js/search.js"></script>
+        <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDyIMWhs-crjT0yhctbRjfJFq75FlEhSzE&callback=initMap">
+        </script>
---
+++ bakery/layout/style.blade.php
@@ -0,0 +1,19 @@
+  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Righteous&display=swap&subset=latin-ext" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+  <!-- Place favicon.ico in the root directory -->
+  <link rel="/bakery/icon" type="/bakery/image/png" href="favicon-32x32.png" sizes="32x32" />
+  <link rel="/bakery/icon" type="/bakery/image/png" href="favicon-16x16.png" sizes="16x16" />
+  <link rel="stylesheet" href="/bakery/css/bootstrap.min.css">
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+  <link rel="stylesheet" href="/bakery/css/owl.carousel.min.css">
+  <link rel="stylesheet" href="/bakery/css/font-awesome.min.css">
+  <link rel="stylesheet" href="/bakery/css/reset.css">
+  <link rel="stylesheet" href="/bakery/css/style.css">
+  <link rel="stylesheet" href="/bakery/css/animate.css">
+  <link rel="stylesheet" href="/bakery/css/responsive.css">
+  <link rel="stylesheet" href="/bakery/css/main.css">
+  <link rel="stylesheet" href="/bakery/css/style-slick.css">
+  <link rel="stylesheet" href="/bakery/css/search.css">
+   <link rel="stylesheet" href="/bakery/css/details.css">
+  <script src="/bakery/js/vendor/modernizr-2.8.3.min.js"></script>
