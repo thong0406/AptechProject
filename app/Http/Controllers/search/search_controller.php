c7ee78d62d3ebe718d9551e9a42b11744f516868
thong0406 <55343500+thong0406@users.noreply.github.com>
2022-01-01 17:23:44 +0700

    Update search_controller.php

--- app/Http/Controllers/search/search_controller.php
+++ app/Http/Controllers/search/search_controller.php
@@ -12,6 +12,7 @@ use App\Models\Book_tags;
 use App\Models\Comments;
 use App\Models\Order_details;
 use App\Models\Bookstores;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class search_controller extends Controller
@@ -25,6 +26,7 @@ class search_controller extends Controller
 		];
 		if ($param['type'] == '') $param['type'] = 'book_name';
 
+
 		$delimeter = ' ';
 		$keywords = explode($delimeter, $param['search']);
 
@@ -33,13 +35,47 @@ class search_controller extends Controller
 			$condition[] = [ $param['type'] , 'like' , '%' .$keyword .'%' ];
 		}
 
-		if (($param['bookstore'] != '-1') and ($param['bookstore'] != '')) $condition[] = [ 'bookstore_id' , '=' , $param['bookstore'] ];\
+		if (($param['bookstore'] != '-1') and ($param['bookstore'] != '')) $condition[] = [ 'bookstore_id' , '=' , $param['bookstore'] ];
 		if (($param['tags'] != '-1') and ($param['tags'] != '')) {
-
+			
 		}
-		$arr = Books::where($condition)->get();
 
-		$books = $arr;
+		$tags_not_empty = 0;
+		if ($param['tags'] != '') {
+			foreach ($param['tags'] as $value) {
+				if ($value != -1) {
+					$tags_not_empty = 1;
+					break;
+				}
+			}
+		}
+		$condition_or = [];
+		if ($tags_not_empty == 1) {
+			$book_tags = DB::table('book_tags');
+			foreach ($param['tags'] as $value) {
+				$book_tags->orWhere('tag_id' , '=' , $value);
+			}
+			foreach ($book_tags->get() as $key => $value) {
+				$condition_or[] = ['id' , '=' , $value->book_id];
+			}
+		}
+		$books = Books::query();
+		foreach ($condition as $value) {
+			$books = $books->where($value[0] , $value[1] , $value[2]);
+		}
+		$books = $books->where(function ($query) use ($condition_or) {
+				foreach ($condition_or as $value) {
+					$query->orWhere($value[0] , $value[1] , $value[2]);
+				}
+			}
+		);
+		/*
+		foreach ($condition_or as $key => $value) {
+			$books = $books->orWhere($value[0] , $value[1] , $value[2]);
+		}
+		*/
+		$books = $books->get();
+
 		foreach ($books as $key => $value) {
 			$bookstores = Bookstores::where("id" , "=" , $value['bookstore_id'])->get();
 			$value['bookstore_name'] = $bookstores[0]['bookstore_name'];
@@ -49,7 +85,7 @@ class search_controller extends Controller
 				$stars += $comment['rating'];
 			}
 			$stars = round($stars/(count($comments)+1));
-			$arr[$key]['stars'] = $stars;
+			$books[$key]['stars'] = $stars;
 		}
 
 		$bookstores = Bookstores::all();
