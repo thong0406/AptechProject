46e67c99166270bd56ad03baea5cc2c2c339ce97
thong0406 <55343500+thong0406@users.noreply.github.com>
2022-01-04 21:08:01 +0700

    Update LoginController.php

--- app/Http/Controllers/login/LoginController.php
+++ app/Http/Controllers/login/LoginController.php
@@ -1,19 +1,59 @@
 <?php
 
-namespace App\Http\Controllers\login;
+namespace App\Http\Controllers\account;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Users;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
+    public function signin()
+    {
+        return view('loginform.view.signin');
+    }
     public function signup()
     {
         return view('loginform.view.signup');
     }
-
-    public function auth_user()
+    public function auth_user (Request $request)
     {
-        return view('loginform.view.signup');
+        $this->validate($request, [
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('username', 'password');
+        if (Auth::attempt($credentials)) {
+                // Đổi thành Route home nếu có
+            return redirect()->route('home')->with('success','Signed in');
+        }
+    }
+    public function create_user (Request $request)
+    {
+        $this->validate($request , [
+            'name' => 'required',
+            'username'=> 'required',
+            'phone' => 'required',
+            'email' => 'required|unique:users|email',
+            'address' => 'required',
+            'pass' => 'required|min:6|max:32',
+            're-pass' => 'same:pass',
+        ] , 
+        [
+            'required' => 'Please fill in your :attribute.'
+        ]);
+
+        Users::create([
+            'username' => $request->username,
+            'password' => bcrypt($request->pass),
+            'name' => $request->name,
+            'address' => $request->address,
+            'phone' => $request->phone,
+            'email' => $request->email
+        ]);
+
+        return redirect()->route('home');
     }
 }
