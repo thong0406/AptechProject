4eb21454eba6c1b5dafba385c903eb310747b1c7
thong0406 <55343500+thong0406@users.noreply.github.com>
2022-01-06 17:31:43 +0700

    Update signin.blade.php

--- resources/views/loginform/view/signin.blade.php
+++ resources/views/loginform/view/signin.blade.php
@@ -3,28 +3,34 @@
     Sign in
 @endsection
 @section('content')
+    
+    @if (session('errors'))
+        @foreach (session('errors')->all() as $key => $message)
+            <p class="bg-success rounded" style="padding: 10px;">{{ $message }}</p>
+        @endforeach
+    @endif
+
     <div class="main">
     <section class="sign-in">
         <div class="container">
             <div class="signin-content">
                 <div class="signin-image">
                     <figure><img src="/login/images/signin-image.jpg" alt="sing up image"></figure>
-                    <a href='{{ route('register') }}' class="signup-image-link">Create an account</a>
+                    <a href='{{ route('signup') }}' class="signup-image-link">Create an account</a>
                     <a href='{{ route('home') }}' class="signup-image-link">Home</a>
                 </div>
 
-                <div class="
-                        signin-form">
+                <div class="signin-form">
                     <h2 class="form-title">Login</h2>
                     <form method="POST" class="register-form" id="login-form" action="{{ route('auth_user') }}">
                     	@csrf
                         <div class="form-group">
                             <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                            <input type="text" name="username" id="your_name" placeholder="Your Username" />
+                            <input type="text" name="username" id="username" placeholder="Your Username" />
                         </div>
                         <div class="form-group">
                             <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                            <input type="password" name="pass" id="your_pass" placeholder="Password" />
+                            <input type="password" name="password" id="password" placeholder="Password" />
                         </div>
                         <div class="form-group">
                             <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
