6a64015b8e203710c23b87014da37167d57bb9dc
thong0406 <55343500+thong0406@users.noreply.github.com>
2022-01-14 15:18:37 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,24 @@ use App\Http\Controllers\account\LoginController;
 use App\Http\Controllers\search\search_controller;
 use App\Http\Controllers\admin\admin_controller;
 
+Route::get('logout' , [LoginController::class , 'logout'])->name('logout');
+	// User Account
+Route::get('signin' , [LoginController::class , 'signin'])->name('login');
+Route::get('signup' , [LoginController::class , 'signup'])->name('signup');
+Route::post('create_user' , [LoginController::class , 'create_user'])->name('create_user');
+Route::post('auth_user', [LoginController::class, 'auth_user'])->name('auth_user');
+	// Admin Account
+Route::get('admin_signin' , [LoginController::class , 'admin_signin'])->name('admin_login');
+//Route::get('admin_signup' , [LoginController::class , 'admin_signup'])->name('admin_signup');
+//Route::post('create_admin' , [LoginController::class , 'create_admin'])->name('create_admin');
+Route::post('auth_admin', [LoginController::class, 'auth_admin'])->name('auth_admin');
+
+Route::group(['prefix'=>'user' , 'middleware'=>'check_user_account'] , function () {
+	Route::get('/settings',[HomeController::class,'user_settings'])->name('user_settings');
+	Route::put('/update_image',[HomeController::class,'user_update_image'])->name('user_update_image');
+	Route::put('/update_details',[HomeController::class,'user_update_details'])->name('user_update_details');
+});
+
 Route::group(['prefix'=>'cart' , 'middleware'=>'check_user_account'] , function () {
 	Route::get('/',[cart_controller::class,'cart'])->name('cart');
 	Route::post('/update/{id}',[cart_controller::class,'cart_update'])->name('cart_update');
@@ -16,15 +34,10 @@ Route::group(['prefix'=>'cart' , 'middleware'=>'check_user_account'] , function 
 	Route::post('/order',[cart_controller::class,'cart_order'])->name('cart_order');
 });
 Route::get('details/{id}', [DetailsController::class, 'details'])->name('book_details');
-Route::post('details/comment/{id}', [DetailsController::class, 'create_comment'])->name('details_create_comment');
-Route::post('add_cart/{id}', [DetailsController::class, 'add_cart'])->name('add_cart');
-Route::post('order_now/{id}', [DetailsController::class, 'order_now'])->name('order_now');
+Route::post('details/comment/{id}', [DetailsController::class, 'create_comment'])->name('details_create_comment')->middleware('check_user_account');
+Route::post('add_cart/{id}', [DetailsController::class, 'add_cart'])->name('add_cart')->middleware('check_user_account');
+Route::post('order_now/{id}', [DetailsController::class, 'order_now'])->name('order_now')->middleware('check_user_account');
 
-Route::get('signin' , [LoginController::class , 'signin'])->name('login');
-Route::get('signup' , [LoginController::class , 'signup'])->name('signup');
-Route::post('create_user' , [LoginController::class , 'create_user'])->name('create_user');
-Route::post('auth_user', [LoginController::class, 'auth_user'])->name('auth_user');
-Route::get('logout' , [LoginController::class , 'logout'])->name('logout');
 
 Route::get('{return?}' , [search_controller::class , 'search_view'])->name('home');
 Route::prefix('home')->group(function () {
@@ -32,21 +45,36 @@ Route::prefix('home')->group(function () {
 	Route::post('store' , [search_controller::class , 'search_store'])->name('search_store');
 });
 
-Route::group(['prefix'=>'admin' /*, 'middleware'=>'check_admin_account'*/] , function () {
+Route::group(['prefix'=>'admin' , 'middleware'=>'check_admin_account'] , function () {
+	Route::get('/settings' , [ admin_controller::class , 'admin_account_settings' ])
+		->name('admin_account_settings');
+	Route::put('/update_image',[admin_controller::class,'admin_account_update_image'])
+		->name('admin_account_update_image');
+	Route::put('/update_details',[admin_controller::class,'admin_account_update_details'])
+		->name('admin_account_update_details');
+	Route::get('/admin_comment_lists',[admin_controller::class,'admin_comment_lists'])
+		->name('admin_comment_lists');
+
+	Route::group(['prefix'=>'admins' , 'middleware'=>['check_admin_account' , 'check_admin_level']] , function () {
+
+		Route::get('/' , [ admin_controller::class , 'admin_admin_lists' ])
+			->name('admin_admin_lists');
+		Route::get('/index' , [ admin_controller::class , 'admin_admin_lists' ])
+			->name('admin_admin_lists');
+		Route::get('/add' , [ admin_controller::class , 'admin_admin_add' ])
+			->name('admin_admin_add');
+		Route::post('/store' , [ admin_controller::class , 'admin_admin_store' ])
+			->name('admin_admin_store');
+		Route::get('/delete/{id}' , [ admin_controller::class , 'admin_admin_delete' ])
+			->name('admin_admin_delete');
+
+	});
 	Route::prefix('users')->group(function () {
 
 		Route::get('/' , [ admin_controller::class , 'admin_user_lists' ])
 			->name('admin_user_lists');
 		Route::get('/index' , [ admin_controller::class , 'admin_user_lists' ])
 			->name('admin_user_lists');
-		Route::get('/add' , [ admin_controller::class , 'admin_user_add' ])
-			->name('admin_user_add');
-		Route::post('/store' , [ admin_controller::class , 'admin_user_store' ])
-			->name('admin_user_store');
-		Route::get('/edit/{id}' , [ admin_controller::class , 'admin_user_edit' ])
-			->name('admin_user_edit');
-		Route::put('/update/{id}' , [ admin_controller::class , 'admin_user_update' ])
-			->name('admin_user_update');
 		Route::get('/delete/{id}' , [ admin_controller::class , 'admin_user_delete' ])
 			->name('admin_user_delete');
 		Route::get('/comments/{id}' , [ admin_controller::class , 'admin_user_comment' ])
@@ -109,9 +137,7 @@ Route::group(['prefix'=>'admin' /*, 'middleware'=>'check_admin_account'*/] , fun
 	});
 	Route::prefix('orders')->group(function () {
 
-		Route::get('/' , [ admin_controller::class , 'admin_order_lists' ])
-			->name('admin_order_lists');
-		Route::get('/index' , [ admin_controller::class , 'admin_order_lists' ])
+		Route::get('/index/{id?}' , [ admin_controller::class , 'admin_order_lists' ])
 			->name('admin_order_lists');
 		Route::get('/delete/{id}' , [ admin_controller::class , 'admin_order_delete' ])
 			->name('admin_order_delete');
